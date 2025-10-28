69aaf04798cc34d290079c64f146faeddc14231c
Mariana <92758993+Got7mj@users.noreply.github.com>
2025-10-28 14:39:02 -0300

    Update page.js

--- receita14/app/novarota/page.js
+++ receita14/app/novarota/page.js
@@ -1,10 +1,20 @@
-import { MariaPrea } from "./componentes";
-export default function NovaRotaHome(){
+import MariaPrea from "./componentes/MariaPrea";
 
-   return (
-      <div>
-         <h1>Nova Rota, Nova Página</h1>
-         <MariaPrea/>
-      </div>
-   )
+export default function NovaRotaHome() {
+  return (
+    <div>
+      <h1>Nova Rota, Nova Página</h1>
+
+      {/* Usando o componente com diferentes parâmetros */}
+      <MariaPrea mensagem="Morreu Maria Preá 😢" cor="red" />
+      <MariaPrea mensagem="Mas ela reviveu em React! ⚛️" cor="green" />
+      <MariaPrea mensagem="E virou componente reutilizável! 💪" cor="blue" />
+
+      <p>Visite as subpáginas:</p>
+      <ul>
+        <li><a href="/novarota/subpagina">Subpágina 1</a></li>
+        <li><a href="/novarota/subpagina2">Subpágina 2</a></li>
+      </ul>
+    </div>
+  );
 }
